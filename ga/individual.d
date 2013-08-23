f02954ece7c63af28049ff11f9e3dd9ac5838a0d
Atila Neves <aalvesne@cisco.com>
2013-08-23 23:03:50 +0200

    Got the xover test to pass

--- ga/individual.d
+++ ga/individual.d
@@ -1,16 +1,36 @@
 module ga.individual;
 
-struct Individual(uint length) {
+import std.algorithm;
+
+
+struct Individual(uint LENGTH) {
 
     alias ubyte allele;
 
-    Individual[] crossover(const ref Individual other, uint pos) const pure nothrow {
-        return [];
+    this(allele[LENGTH] genome) {
+        _genome = genome;
     }
 
-    @property allele[] genome() const pure nothrow { return []; }
+    Individual[2] crossover(const ref Individual other, uint pos) const {
+        auto child1 = Individual!LENGTH();
+        auto child2 = Individual!LENGTH();
+
+        for(int i = 0; i < pos; ++i) {
+            child1._genome[i] = this._genome[i];
+            child2._genome[i] = other._genome[i];
+        }
+
+        for(int i = pos; i < LENGTH; ++i) {
+            child2._genome[i] = this._genome[i];
+            child1._genome[i] = other._genome[i];
+        }
+
+        return [child1, child2];
+    }
+
+    @property allele[LENGTH] genome() const pure nothrow { return _genome; }
 
 private:
 
-    allele[] _genome;
+    allele[LENGTH] _genome;
 }
